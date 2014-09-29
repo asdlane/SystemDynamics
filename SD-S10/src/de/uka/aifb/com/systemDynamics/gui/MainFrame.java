b98f7767d238438f462aa3f681a59f4aa38616d0
John Hinkel <jhinkel3@gmail.com>
2014-09-28 20:08:35 -0400

    Cut, Copy, and Paste work, but there are a few problems.  TheImageIcon doesn't display, and when a node is cut/copied and pasted, it cannot be removed from the graph.  working to address those issues

--- SD-S10/src/de/uka/aifb/com/systemDynamics/gui/MainFrame.java
+++ SD-S10/src/de/uka/aifb/com/systemDynamics/gui/MainFrame.java
@@ -30,6 +30,7 @@ import de.uka.aifb.com.systemDynamics.xml.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.net.URL;
 import java.util.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -74,6 +75,7 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
    
    private static final String FILE_NEW_ICON = "resources/page_white.png";
    private static final String FILE_OPEN_ICON = "resources/folder_page_white.png";
+   private static final String COPY_ICON = "resources/scissors.png";
    private static final String FILE_SAVE_ICON = "resources/disk.png";
    
    private static final String FILE_NEW_AN_ICON = "resources/new_auxiliary_node_en_US.png";
@@ -130,6 +132,9 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
    private Action zoomStandardAction;
    private Action zoomInAction;
    private Action zoomOutAction;
+   private Action cut;
+   private Action copy;
+   private Action paste;
    private JCheckBoxMenuItem addFlowModeCheckBoxMenuItem;
    private JRadioButtonMenuItem rbMenuItemEnglish;
    private JRadioButtonMenuItem rbMenuItemGerman;
@@ -148,6 +153,7 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
          throw new IllegalArgumentException("'start' must not be null.");
       }
       
+ 
       this.start = start;
       messages = start.getMessages();
       
@@ -180,10 +186,12 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
       contentPanel = new JPanel(new BorderLayout());
       getContentPane().add(contentPanel, BorderLayout.CENTER);
       registerDelAction();
+
       setVisible(true);
 
       setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
       addWindowListener(this);
+
    }
    
    //ADDED BY JOHN HINKEL.  The registerDelAction and askconfirm methods
@@ -239,7 +247,15 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
        // Get input map when fileChooser is in focused window and put a keystroke DELETE
        // associate the key stroke (DELETE) (here) with "delAction"
        fileChooser.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("DELETE"),"delAction");
+       
    }
+   
+   
+   //*********************************STUFF THAT COULD HELP WITH CUT, COPY, PASTE*************************************************
+   //graph.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke('p',InputEvent.CTRL_MASK), actionMapKey);
+   //javax.swing.TransferHandler.getCutAction();
+   //javax.swing.TransferHandler.getCopyAction();
+   //javax.swing.TransferHandler.getPasteAction();
    
    public int askConfirm()
    {
@@ -629,7 +645,38 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
       toolBar.add(zoomStandardAction);
       toolBar.add(zoomInAction);
       toolBar.add(zoomOutAction);
+      Action action;
       
+      // Cut Action
+      action = javax.swing.TransferHandler.getCutAction();
+      ImageIcon CutIcon = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource(COPY_ICON), "Cut");
+      action.putValue(Action.LARGE_ICON_KEY, CutIcon);
+      cut = new EventRedirector(action);
+      toolBar.add(cut);
+      System.out.println(CutIcon.getIconHeight());
+      System.out.println(CutIcon.getIconWidth());
+      System.out.println();
+      
+      // Copy Action
+      action = javax.swing.TransferHandler.getCopyAction();    
+      ImageIcon CopyIcon = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource(COPY_ICON), "Copy");    		  
+      action.putValue(Action.LARGE_ICON_KEY, CopyIcon);
+      copy = new EventRedirector(action);
+      toolBar.add(copy);
+      System.out.println(CopyIcon.getIconHeight());
+      System.out.println(CopyIcon.getIconWidth());
+      System.out.println();
+      
+      // Paste Action
+      action = javax.swing.TransferHandler.getPasteAction();
+      ImageIcon PasteIcon = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource(COPY_ICON), "Paste");
+      System.out.println(PasteIcon.getIconHeight());
+      System.out.println(PasteIcon.getIconWidth());
+	  action.putValue(Action.SMALL_ICON, PasteIcon);
+	  paste = new EventRedirector(action);
+	  toolBar.add(paste);
+      
+	  
       return toolBar;
    }
    
@@ -803,6 +850,7 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
          super(name, icon);
          
          putValue(Action.SHORT_DESCRIPTION, toolTipText);
+
       }
       
       public void actionPerformed(ActionEvent e) {
@@ -813,6 +861,7 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
                                                 "");
          
          graph = new SystemDynamicsGraph(start, MainFrame.this);
+
          if (modelName != null) {
         	 DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss z");
         	 Date today = Calendar.getInstance().getTime();
@@ -848,7 +897,9 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
             zoomInAction.setEnabled(true);
             zoomOutAction.setEnabled(true);
          }
+
       }
+      
    }
    
    private class OpenAction extends AbstractAction {
@@ -869,6 +920,7 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
             try {
                graph = XMLModelReader.readXMLSystemDynamicsGraph(file.getAbsolutePath(), start, MainFrame.this);
                graph.addSystemDynamicsGraphModifiedEventListener(MainFrame.this);
+               
             } catch (AuxiliaryNodesCycleDependencyException excep) {
                JOptionPane.showMessageDialog(MainFrame.this,
                                              messages.getString("MainFrame.OpenFile.AuxiliaryNodesCycleDependencyException.Text"),
@@ -1532,4 +1584,22 @@ public class MainFrame extends JFrame implements ActionListener, ChangeListener,
          setTitle(createTitle(graph.getModelName(), graphModified));
       }
    }
+   // This will change the source of the actionevent to graph.
+	public class EventRedirector extends AbstractAction {
+
+		protected Action action;
+
+		// Construct the "Wrapper" Action
+		public EventRedirector(Action a) {
+			super("", (ImageIcon) a.getValue(Action.SMALL_ICON));
+			this.action = a;
+		}
+
+		// Redirect the Actionevent
+		public void actionPerformed(ActionEvent e) {
+			e = new ActionEvent(graph, e.getID(), e.getActionCommand(), e
+					.getModifiers());
+			action.actionPerformed(e);
+		}
+	}
 }
